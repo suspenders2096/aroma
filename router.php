688d122da0fed95614ebc3c038f99f4ebcc72d56
Ekaterina Golubchik <55327238+suspenders2096@users.noreply.github.com>
2022-05-17 13:04:30 +0300

    Обновленные маршруты

--- router.php
+++ router.php
@@ -1,20 +1,32 @@
 <?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
 
 $url = explode('/', $_SERVER['REQUEST_URI']);
+require_once("php/classes/Users.php");
+require_once("php/db.php");
 
-if($url[1] == "auth") {
+if ($url[1] == "auth") {
     $content = file_get_contents("pages/login.html");
-} else if ($url[1] == "reg") {
+  } else if ($url[1] == "reg") {
     $content = file_get_contents("pages/register.html");
-} else if ($url[1] == "blog") {
+  } else if ($url[1] == "blog") {
     $content = file_get_contents("pages/blog.html");
-} else if ($url[1] == "users") {
+  }else if ($url[1] == "users") {
+  require_once("pages/users/index.php");
+  } else if ($url[1] == "users") {
     require_once("pages/users/index.php");
-} else if ($url[1] == "contact") {
-    $content = file_get_contents("contact.html");
-} else {
+  } else if ($url[1] == "addUser") {
+    echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"],$_POST["password"]);
+  } else if ($url[1] == "authUser") {
+    echo User::authUser($_POST["email"], $_POST["password"]);
+  } else if ($url[1] == "getUser") {
+    echo User::getUser($_SESSION["id"]);
+  } else if ($url[1] == "getUsers") {
+    echo User::getUsers();
+  } else {
     $content = file_get_contents("pages/index.php");
-}
+  }
 
 if (!empty($content)) {
     require_once("template.php");
